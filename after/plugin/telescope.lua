f1cec575e3f8332c49800fd5a57a3056f70e9f8a
Ben Brackenbury <benbrack22@icloud.com>
2024-03-29 07:15:40 +0000

    style: adjust colors

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -4,3 +4,7 @@ vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ps', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
+
+vim.keymap.set('n', '<leader>ccs', function()
+	builtin.colorscheme({enable_preview = true});
+end)
