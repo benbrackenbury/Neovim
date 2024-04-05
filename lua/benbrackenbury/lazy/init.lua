108fbfd22895582f596a8793bf0dbb2f8465d634
Ben Brackenbury <benbrack22@icloud.com>
2024-04-05 07:46:05 +0100

    [wip] migrate from packer to lazy

--- lua/benbrackenbury/lazy/init.lua
+++ lua/benbrackenbury/lazy/init.lua
@@ -1,27 +1,12 @@
--- bootstrap lazy.nvim
-
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
-end
-vim.opt.rtp:prepend(lazypath)
-
 require('lazy').setup({
   {
     'nvim-telescope/telescope.nvim',
-    tag = '0.1.6',
-    requires = { 'nvim-lua/plenary.nvim' }
+    version = '0.1.6',
+    dependencies = { 'nvim-lua/plenary.nvim' }
   },
   {
     "rose-pine/neovim",
-    as = 'rose-pine',
+    name = 'rose-pine',
     config = function()
       vim.cmd('colorscheme rose-pine')
     end
@@ -39,7 +24,7 @@ require('lazy').setup({
   {
     'VonHeikemen/lsp-zero.nvim',
     branch = 'v3.x',
-    requires = {
+    dependencies = {
       'williamboman/mason.nvim',
       'williamboman/mason-lspconfig.nvim',
       'neovim/nvim-lspconfig',
