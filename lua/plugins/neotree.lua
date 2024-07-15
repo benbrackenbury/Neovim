b54f60c203691e0822849a541f0ea652721c1e09
Ben Brackenbury <benbrack22@icloud.com>
2024-07-15 23:02:33 +0100

    feat: neotree follow current file

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -2,11 +2,18 @@ return {
     "nvim-neo-tree/neo-tree.nvim",
     branch = "v3.x",
     dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons",
-      "MunifTanjim/nui.nvim",
+        "nvim-lua/plenary.nvim",
+        "nvim-tree/nvim-web-devicons",
+        "MunifTanjim/nui.nvim",
     },
     config = function()
-       vim.keymap.set("n", "<leader>e", ":Neotree toggle<CR>")
+        vim.keymap.set("n", "<leader>e", ":Neotree toggle<CR>")
+        require("neo-tree").setup {
+            filesystem = {
+                follow_current_file = {
+                    enabled = true,
+                },
+            },
+        }
     end
 }
