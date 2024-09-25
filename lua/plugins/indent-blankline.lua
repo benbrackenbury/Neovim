20f2b4282c0b010f54ce6adf38c916d48ba4beb3
Ben Brackenbury <benbrack22@icloud.com>
2024-09-25 21:21:36 +0100

    feat: add indent-blankline plugin
    https://github.com/echasnovski/mini.indentscope

---
+++ lua/plugins/indent-blankline.lua
@@ -0,0 +1,15 @@
+return {
+    "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
+    ---@module "ibl"
+    ---@diagnostic disable-next-line: undefined-doc-name
+    ---@type ibl.config
+    opts = {},
+    config = function()
+        require("ibl").setup {
+            scope = {
+                enabled = false,
+            },
+        }
+    end,
+}
