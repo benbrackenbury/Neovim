8bc03ed464b8194bef942ca5116eba0163a74eb8
Ben Brackenbury <benbrack22@icloud.com>
2024-12-11 08:22:42 +0000

    Update parrot.lua

--- lua/plugins/parrot.lua
+++ lua/plugins/parrot.lua
@@ -7,7 +7,7 @@ return {
             -- Providers must be explicitly added to make them available.
             providers = {
                 xai = {
-                    api_key = "xai-i6y0ICFOVzolKVQr2hiDv9BsZBgsH75zDhnyH7mAkZjoP11ohuSH9TNZDtnX1j7N3Kdol3p1vxrh38oZ",
+                    api_key = "",
                 },
             },
         }
