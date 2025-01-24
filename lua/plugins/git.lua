4ecf5336af04f8f6cedf742a057651c52b49249f
Ben Brackenbury <benbrack22@icloud.com>
2025-01-24 07:48:25 +0000

    feat: git fugitive keymaps for conflicts

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -9,6 +9,8 @@ return {
         "tpope/vim-fugitive",
         config = function()
             vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+            vim.keymap.set("n", "<leader>gh", "<cmd>diffget //2<CR>")
+            vim.keymap.set("n", "<leader>gl", "<cmd>diffget //3<CR>")
         end
     }
 }
