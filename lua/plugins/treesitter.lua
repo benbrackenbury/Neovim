99f712138604865e77afad699daff5bbd821a331
Ben Brackenbury <benbrack22@icloud.com>
2025-01-24 07:37:40 +0000

    feat: add treesitter-context

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,87 +1,96 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    dependencies = {
-        'nvim-treesitter/nvim-treesitter-textobjects',
-    },
-    config = function()
-        require 'nvim-treesitter.configs'.setup({
-            ensure_installed = {
-                "javascript",
-                "typescript",
-                "go",
-                "html",
-                "php",
-                "c",
-                "lua",
-                "vim",
-                "vimdoc",
-                "query",
-            },
-            indent = {
-                enable = true,
-            },
-            sync_install = false,
-            auto_install = true,
-            highlight = {
-                enable = true,
-                disable = { "swift" },
-                additional_vim_regex_highlighting = false,
-            },
-            textobjects = {
-                select = {
+    {
+        "nvim-treesitter/nvim-treesitter",
+        dependencies = {
+            'nvim-treesitter/nvim-treesitter-textobjects',
+        },
+        config = function()
+            require 'nvim-treesitter.configs'.setup({
+                ensure_installed = {
+                    "javascript",
+                    "typescript",
+                    "go",
+                    "html",
+                    "php",
+                    "c",
+                    "lua",
+                    "vim",
+                    "vimdoc",
+                    "query",
+                },
+                indent = {
                     enable = true,
-                    lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
-                    keymaps = {
-                        -- You can use the capture groups defined in textobjects.scm
-                        ['aa'] = '@parameter.outer',
-                        ['ia'] = '@parameter.inner',
-                        ['af'] = '@function.outer',
-                        ['if'] = '@function.inner',
-                        ['ac'] = '@class.outer',
-                        ['ic'] = '@class.inner',
-                        ['ii'] = '@conditional.inner',
-                        ['ai'] = '@conditional.outer',
-                        ['il'] = '@loop.inner',
-                        ['al'] = '@loop.outer',
-                        ['at'] = '@comment.outer',
+                },
+                sync_install = false,
+                auto_install = true,
+                highlight = {
+                    enable = true,
+                    disable = { "swift" },
+                    additional_vim_regex_highlighting = false,
+                },
+                textobjects = {
+                    select = {
+                        enable = true,
+                        lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+                        keymaps = {
+                            -- You can use the capture groups defined in textobjects.scm
+                            ['aa'] = '@parameter.outer',
+                            ['ia'] = '@parameter.inner',
+                            ['af'] = '@function.outer',
+                            ['if'] = '@function.inner',
+                            ['ac'] = '@class.outer',
+                            ['ic'] = '@class.inner',
+                            ['ii'] = '@conditional.inner',
+                            ['ai'] = '@conditional.outer',
+                            ['il'] = '@loop.inner',
+                            ['al'] = '@loop.outer',
+                            ['at'] = '@comment.outer',
+                        },
+                    },
+                    move = {
+                        enable = true,
+                        set_jumps = true, -- whether to set jumps in the jumplist
+                        goto_next_start = {
+                            [']f'] = '@function.outer',
+                            [']]'] = '@class.outer',
+                        },
+                        goto_next_end = {
+                            [']F'] = '@function.outer',
+                            [']['] = '@class.outer',
+                        },
+                        goto_previous_start = {
+                            ['[f'] = '@function.outer',
+                            ['[['] = '@class.outer',
+                        },
+                        goto_previous_end = {
+                            ['[F'] = '@function.outer',
+                            ['[]'] = '@class.outer',
+                        },
                     },
                 },
-                move = {
-                    enable = true,
-                    set_jumps = true, -- whether to set jumps in the jumplist
-                    goto_next_start = {
-                        [']f'] = '@function.outer',
-                        [']]'] = '@class.outer',
-                    },
-                    goto_next_end = {
-                        [']F'] = '@function.outer',
-                        [']['] = '@class.outer',
-                    },
-                    goto_previous_start = {
-                        ['[f'] = '@function.outer',
-                        ['[['] = '@class.outer',
-                    },
-                    goto_previous_end = {
-                        ['[F'] = '@function.outer',
-                        ['[]'] = '@class.outer',
-                    },
-                },
-            },
-        })
+            })
 
-        local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
-        parser_config.blade = {
-            install_info = {
-                url = "https://github.com/EmranMR/tree-sitter-blade",
-                files = { "src/parser.c" },
-                branch = "main",
-            },
-            filetype = "blade",
-        }
-        vim.filetype.add({
-            pattern = {
-                [".*%.blade%.php"] = "blade",
-            },
-        })
-    end
+            local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
+            parser_config.blade = {
+                install_info = {
+                    url = "https://github.com/EmranMR/tree-sitter-blade",
+                    files = { "src/parser.c" },
+                    branch = "main",
+                },
+                filetype = "blade",
+            }
+            vim.filetype.add({
+                pattern = {
+                    [".*%.blade%.php"] = "blade",
+                },
+            })
+        end
+    },
+
+    {
+        'nvim-treesitter/nvim-treesitter-context',
+        config = function ()
+            require('treesitter-context').setup({})
+        end
+    }
 }
