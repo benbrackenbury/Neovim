7e82620f235adc96dfbd1ff573288b75caf6f000
Ben Brackenbury <benbrack22@icloud.com>
2026-05-07 20:00:00 +0100

    style: change to Vercel theme

--- lua/benbrackenbury/appearance.lua
+++ lua/benbrackenbury/appearance.lua
@@ -1,21 +1,44 @@
 vim.pack.add({
 	"https://github.com/xiyaowong/transparent.nvim",
 	"https://github.com/rose-pine/neovim",
+	"https://github.com/tiesen243/vercel.nvim",
+	"https://github.com/vague-theme/vague.nvim",
 })
 
-vim.pack.add({ 'https://github.com/vague-theme/vague.nvim' })
-vim.cmd.colorscheme('vague')
+require("vercel").setup({
+	transparent = true,
+})
+vim.cmd.colorscheme("vercel")
 
 require("transparent").setup({
 	groups = {
-		'Normal', 'NormalNC', 'Comment', 'Constant', 'Special', 'Identifier',
-		'Statement', 'PreProc', 'Type', 'Underlined', 'Todo', 'String', 'Function',
-		'Conditional', 'Repeat', 'Operator', 'Structure', 'LineNr', 'NonText',
-		'SignColumn', 'StatusLine', 'StatusLineNC',
-		'EndOfBuffer', 'TreesitterContext'
+		"Normal",
+		"NormalNC",
+		"Comment",
+		"Constant",
+		"Special",
+		"Identifier",
+		"Statement",
+		"PreProc",
+		"Type",
+		"Underlined",
+		"Todo",
+		"String",
+		"Function",
+		"Conditional",
+		"Repeat",
+		"Operator",
+		"Structure",
+		"LineNr",
+		"NonText",
+		"SignColumn",
+		"StatusLine",
+		"StatusLineNC",
+		"EndOfBuffer",
+		"TreesitterContext",
 	},
 	excludeGroups = {
-		'CursorLine',
-		'CursorLineNr',
-	}
+		"CursorLine",
+		"CursorLineNr",
+	},
 })
