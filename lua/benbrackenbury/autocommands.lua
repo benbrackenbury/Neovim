129ffcc36682b8a82f7d54a8a99a8505b3eada03
Ben Brackenbury <benbrack22@icloud.com>
2026-06-20 09:26:46 +0100

    fix(autocommands): remove duplicate LspAttach and update yank highlight

--- lua/benbrackenbury/autocommands.lua
+++ lua/benbrackenbury/autocommands.lua
@@ -1,18 +1,9 @@
-vim.api.nvim_create_autocmd('LspAttach', {
-    callback = function(ev)
-        local client = vim.lsp.get_client_by_id(ev.data.client_id)
-        if client:supports_method('textDocument/completion') then
-            vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
-        end
-    end,
-})
-
 vim.api.nvim_create_autocmd('TextYankPost', {
     group = vim.api.nvim_create_augroup('highlight_yank', {}),
-    desc = 'Hightlight selection on yank',
+    desc = 'Highlight selection on yank',
     pattern = '*',
     callback = function()
-        vim.highlight.on_yank { higroup = 'IncSearch', timeout = 500 }
+        vim.hl.on_yank { higroup = 'IncSearch', timeout = 500 }
     end,
 })
 
